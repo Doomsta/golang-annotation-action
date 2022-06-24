561a964e86a042da48def0ccd70ea91aa7f0bdcf
André Randel <arandel@freicon.de>
2022-06-24 11:50:15 +0200

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    core.info(`::error file=mosd/internal/model/host.go,line=173}::awesome comment`);
+    core.info(`::error file=mosd/internal/model/host.go,line=173,col=5::awesome comment`);
 } catch (error) {
     core.setFailed(error.message);
 }
