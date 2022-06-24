59cd24619c82a95452fd79c987b535fa6051eed2
André Randel <arandel@freicon.de>
2022-06-24 15:12:43 +0200

    test

--- index.js
+++ index.js
@@ -3,18 +3,33 @@ const github = require('@actions/github');
 const fs = require('fs');
 const lineReader = require('line-by-line');
 
-let file = 'mosd/staticcheck.json'
-if (!fs.existsSync(file)) {
-    core.warning(
-        `No file was found with the provided path: ${file}.`
-    )
-    return
-}
+(function () {
+    let file = 'mosd/staticcheck.json'
+    if (!fs.existsSync(file)) {
+        core.warning(
+            `No file was found with the provided path: ${file}.`
+        )
+        return
+    }
 
+    let lr = new lineReader(file);
+    lr.on('line', function (line) {
+        const currentLine = JSON.parse(line);
+        core.info(`::error file=${currentLine.location.file},line=${currentLine.location.line},col=${currentLine.location.column}::${currentLine.message}`);
+    });
+})();
 
-let lr = new lineReader(file);
-lr.on('line', function (line) {
-    const currentLine = JSON.parse(line);
-    core.info(`::error file=${currentLine.location.file},line=${currentLine.location.line},col=${currentLine.location.column}::${currentLine.message}`);
-});
+(function () {
+    let file = 'yaml-lint.txt'
+    if (!fs.existsSync(file)) {
+        core.warning(`No file was found with the provided path: ${file}.`)
+        return
+    }
+    const rgx = /^(\w+):\s([^:]+):(\d+):(\d+): (.+)$/;
 
+    let lr = new lineReader(file);
+    lr.on('line', function (line) {
+        const matches = rgx.exec(line)
+        core.info(`::error file=${matches[2]},line=${matches[3]},col=${matches[4]}::${matches[5]}`);
+    });
+})();
