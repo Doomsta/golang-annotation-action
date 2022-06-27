506c47a8e4c42226e93a60c07ebe5a582e6782f2
AndreR <arandel@freicon.de>
2022-06-27 09:30:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,22 +18,3 @@ const lineReader = require('line-by-line');
         core.info(`::error file=${currentLine.location.file},line=${currentLine.location.line},col=${currentLine.location.column}::${currentLine.message}`);
     });
 })();
-
-(function () {
-    let file = 'yaml-lint.txt'
-    if (!fs.existsSync(file)) {
-        core.warning(`No file was found with the provided path: ${file}.`)
-        return
-    }
-    const rgx = /(\w+):\s([^:]+):(\d+):(\d+): (.+)/gi;
-
-    let lr = new lineReader(file);
-    lr.on('line', function (line) {
-        const matches = rgx.exec(line)
-        if (!matches ) {
-            core.warning(`skip: ${line}.`)
-            return
-        }
-        core.info(`::error file=${matches[2]},line=${matches[3]},col=${matches[4]}::${matches[5]}`);
-    });
-})();
